a39cb9f3db18d234141dee877e98e46434c2e3ad
Varon Atarrsh U <108916884+Atarrsh18@users.noreply.github.com>
2023-03-31 09:13:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import File from './File';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <File/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
